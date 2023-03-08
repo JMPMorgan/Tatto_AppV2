7ccc5587b23e38428150a414118b2cf1d2bee0a5
JMPMorgan <jonathan.morganp@gmail.com>
2023-03-07 22:47:22 -0600

    MainActivity Changes

--- app/src/main/java/com/example/tattoapp/MainActivity.kt
+++ app/src/main/java/com/example/tattoapp/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : AppCompatActivity() {
 //        val local= Local()
 
         supportFragmentManager.commit {
-            replace <LocalsFragment> (R.id.fragment_container)
+            replace  (R.id.fragment_container,<LocalsFragment>)
             setReorderingAllowed(true)
             addToBackStack("replacement")
         }
