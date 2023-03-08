ca2086af31e0187a68a2cc72374b4d443b267900
JMPMorgan <jonathan.morganp@gmail.com>
2023-03-08 15:56:47 -0600

    Changes in Fragments main Activity

--- app/src/main/java/com/example/tattoapp/MainActivity.kt
+++ app/src/main/java/com/example/tattoapp/MainActivity.kt
@@ -7,13 +7,48 @@ import android.util.Log
 import androidx.fragment.app.*
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tattoapp.Fragments.LocalsFragment
+import com.example.tattoapp.Fragments.ProfileFragment
+import com.example.tattoapp.Fragments.SearchFragment
 import com.example.tattoapp.Models.Local
 import com.example.tattoapp.RecyclerViews.LocalRecyclerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var  recyclerView: RecyclerView
     private lateinit var adapter:LocalRecyclerView
+
+    lateinit var  bottomNavigation:BottomNavigationView
+    private val onNavMenu= BottomNavigationView.OnNavigationItemSelectedListener{
+        item->
+        when(item.itemId){
+            R.id.LocalsFragmentOptions->{
+                supportFragmentManager.commit {
+                    replace<LocalsFragment> (R.id.fragment_container)
+                    setReorderingAllowed(true)
+                    addToBackStack("replacement")
+                }
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.PruebaOptions->{
+                supportFragmentManager.commit {
+                    replace<ProfileFragment> (R.id.fragment_container)
+                    setReorderingAllowed(true)
+                    addToBackStack("replacement")
+                }
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.PruebaOptions2->{
+                supportFragmentManager.commit {
+                    replace<SearchFragment> (R.id.fragment_container)
+                    setReorderingAllowed(true)
+                    addToBackStack("replacement")
+                }
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
 
 
 
@@ -23,6 +58,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 //        val local= Local()
 
+        bottomNavigation=findViewById(R.id.navMenu)
+        bottomNavigation.setOnNavigationItemSelectedListener(onNavMenu)
         supportFragmentManager.commit {
             replace<LocalsFragment> (R.id.fragment_container)
             setReorderingAllowed(true)
