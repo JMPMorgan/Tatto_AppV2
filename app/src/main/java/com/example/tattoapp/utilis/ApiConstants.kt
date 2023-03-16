50ccaab878563e37804ecca560f3620496b83fa3
JMPMorgan <jonathan.morganp@gmail.com>
2023-03-15 22:45:30 -0600

    Profile Fragment Init

--- app/src/main/java/com/example/tattoapp/utilis/ApiConstants.kt
+++ app/src/main/java/com/example/tattoapp/utilis/ApiConstants.kt
@@ -4,7 +4,8 @@ class ApiConstants {
     companion object{
         //Esta ip es la ip de tu computadora para saberla en CMD pon ipconfig
 //        const val BASEURL_DEV="https://backend-tatto.onrender.com/"
-        const val BASEURL_DEV="http://192.168.100.8:8080"
+        //192.168.100
+        const val BASEURL_DEV="http://172.20.11.24:8080"
         const val BASEURL_PROD=""
     }
 }
